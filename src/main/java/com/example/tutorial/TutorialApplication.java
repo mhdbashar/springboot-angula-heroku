bfb7067bdf7ec1bc563e4e1c00b8eada9b4f0fbe
MhdBashar <mhdbashard@gmail.com>
2021-10-27 13:13:49 +0300

    update conf

--- src/main/java/com/example/tutorial/TutorialApplication.java
+++ src/main/java/com/example/tutorial/TutorialApplication.java
@@ -2,11 +2,9 @@ package com.example.tutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 
-@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@SpringBootApplication
 public class TutorialApplication {
 
 	public static void main(String[] args) {
