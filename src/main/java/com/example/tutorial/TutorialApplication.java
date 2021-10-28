dfe9247213de820994c88a26ba4529e167402281
MhdBashar <mhdbashard@gmail.com>
2021-10-28 11:37:58 +0300

    add swagger for API Documentation

--- src/main/java/com/example/tutorial/TutorialApplication.java
+++ src/main/java/com/example/tutorial/TutorialApplication.java
@@ -1,13 +1,37 @@
 package com.example.tutorial;
 
+import java.io.FileReader;
+import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.PathSelectors;
 
 @SpringBootApplication
+@EnableSwagger2
 public class TutorialApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TutorialApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialApplication.class, args);
+    }
+
+    @Bean
+    public Docket api() throws IOException {
+        ApiInfoBuilder builder = new ApiInfoBuilder()
+                .title("Tutorial Service Api Documentation")
+                .description("Documentation automatically generated");
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.tutorial"))
+                .paths(PathSelectors.any()).build()
+                .apiInfo(builder.build());
+    }
 }
